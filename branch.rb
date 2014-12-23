9d2331d43cdad80f0fd90d102a529c3243b3cdcf
Justin Lambert <jlambert@au.westfield.com>
2014-12-24 09:52:43 +1100

    tweak logic

--- branch.rb
+++ branch.rb
@@ -1,3 +1,5 @@
-first = "git checkout -b branch-#{Time.now.to_i}"
-second = "git commit -am \"#{ARGV.first}\""
+message = ARGV.first
+name = message.gsub(/[^0-9a-z]/i, '-')
+first = "git checkout -b #{name}-#{Time.now.to_i}"
+second = "git commit -am \"#{message}\""
 `#{first} && #{second}`
